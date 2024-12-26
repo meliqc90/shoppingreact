6e9193822602d0d85b7f56416360b7800a92a5d8
meliqc <meliqc90@gmail.com>
2024-12-26 12:38:14 -0500

    Actualizar configuración y archivos para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
   base: "/shoppingreact",
   plugins: [react()],
-})
+});
